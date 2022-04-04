9f922bf8120baeb6c6b9ce66ccc577fd0b9dd182
Elik13 <gbccasaloma@yahoo.com>
2022-04-03 21:22:54 -0400

    Admin sees user profiles and listsings, user - only listings; Listings model and service

---
+++ src/app/models/listing.ts
@@ -0,0 +1,7 @@
+export interface Listing {
+    listing_id: number,
+    listing_title: string,
+    description: string,
+    street: string,
+    city: string
+}
---
+++ src/app/services/listing.service.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '@/models';
+
+@Injectable({ providedIn: 'root' })
+export class ListingService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${config.apiUrl}/listings`);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${config.apiUrl}/listings/${id}`);
+    }
+}
